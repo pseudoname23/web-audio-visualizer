98abdc29da45e30d815ff24cedbb9d375286c4aa
pseudoname <90123698+pseudoname23@users.noreply.github.com>
2022-01-31 10:27:00 -0600

    Test if glitch will automatically pull changes

--- scripts/misc.js
+++ scripts/misc.js
@@ -16,3 +16,7 @@ function HEXtoRGBA(hex, a) {
 function isWithin(x, y, box) {
   return (x>box[0]?(y>box[1]?(x-box[0]<box[2]?(y-box[1]<box[3]):false):false):false)
 } // Why does this work. I hate myself
+
+function doesGlitchPull() {
+  return true;
+}
